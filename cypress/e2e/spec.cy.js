2d599489c8416f7295c8e56eb2c2534e4335ba45
GustavoTex4 <gustavo472017@hotmail.com>
2023-06-13 21:59:30 -0300

    atividade cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('testando aluno online ', () => {
+  it('novo requerimento', () => {
+    cy.visit('http://localhost:3000/requerimentos')
+    cy.get('.btn').click()
+    cy.get('#inputDisciplina').select("ADS030 - Manutenção de Software e DevOps")
+    cy.get('#inputProva').select("A1")
+    cy.get('#inputArgumentacao').type('Teste Cypress')
+
   })
 })
+document.querySelector("#inputMatricula")
